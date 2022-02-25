e1a5c7620c81d5e43bd2aee3d3a833a966b27714
Koose <KooseTheBlogger@gmail.com>
2022-02-25 13:30:01 +0000

    adapter error

--- app/src/main/java/com/koose/highwayapp/Onboading/IntroSlideAdapter.kt
+++ app/src/main/java/com/koose/highwayapp/Onboading/IntroSlideAdapter.kt
@@ -1,2 +1,28 @@
 package com.koose.highwayapp.Onboading
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class IntroSlideAdapter(private val introSlide: List<IntroSlide>):
+                RecyclerView.Adapter<IntroSlideAdapter.IntroSliderViewHolder>(){
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IntroSliderViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: IntroSliderViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class IntroSliderViewHolder(view: View) {
+        private val textTitle:
+
+    }
+}
