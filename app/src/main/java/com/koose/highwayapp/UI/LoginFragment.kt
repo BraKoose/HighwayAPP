287f3d974c8cda1d0e16c521e47d4137f291deed
Koose <KooseTheBlogger@gmail.com>
2022-02-25 17:06:18 +0000

    fragment error

--- app/src/main/java/com/koose/highwayapp/UI/LoginFragment.kt
+++ app/src/main/java/com/koose/highwayapp/UI/LoginFragment.kt
@@ -7,26 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.koose.highwayapp.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
+    private lateinit var binding:LoginFragment
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
