50b3a341bb1155469a3638382f5246a53f82c218
tifuseini <tifuseini@st.ug.edu.gh>
2022-02-25 15:21:17 +0000

    IntroBuuz

--- app/src/main/java/com/koose/highwayapp/IntroBuz.kt
+++ app/src/main/java/com/koose/highwayapp/IntroBuz.kt
@@ -1,2 +1,123 @@
 package com.koose.highwayapp
 
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.viewpager2.widget.ViewPager2
+
+class IntroBuz : AppCompatActivity() {
+
+    private lateinit var introSliderViewPager : ViewPager2
+    private lateinit var button: Button
+    private lateinit var skipIntroText : TextView
+    private lateinit var indicatorsContainer : LinearLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        introSliderViewPager = findViewById(R.id.introSliderViewPager)
+        button = findViewById(R.id.buttonNext)
+        introSliderViewPager.adapter = introSliderViewPager
+        skipIntroText = findViewById(R.id.skipIntroText)
+        indicatorsContainer = findViewById(R.id.indicatorsContainer)
+
+        setupIndicators()
+        setCurrentIndicators(0)
+
+        introSliderViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                setCurrentIndicators(position)
+            }
+        })
+
+        button.setOnClickListener {
+            if (introSliderViewPager.currentItem + 1 < introSliderAdapter.itemCount) {
+                introSliderViewPager.currentItem +=1
+            }else{
+                Intent(applicationContext, MainActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+        }
+
+        skipIntroText.setOnClickListener {
+            Intent(applicationContext, MainActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+
+
+
+
+    }
+
+    private fun setupIndicators() {
+        val indicators = arrayOfNulls<ImageView>(introSliderAdapter.itemCount)
+        val layoutParams: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+        layoutParams.setMargins(8, 0, 8, 0)
+        for (i in indicators.indices) {
+            indicators[i] = ImageView(applicationContext)
+            indicators[i].apply {
+                this?.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive
+                    )
+                )
+                this?.layoutParams = layoutParams
+            }
+            indicatorsContainer.addView(indicators[i])
+
+        }
+    }
+
+    private fun setCurrentIndicators(index : Int) {
+        val childCount = indicatorsContainer.childCount
+        for (i in 0 until childCount) {
+            val imageView =
+                indicatorsContainer[i] as ImageView
+            if (i == index) {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_active
+                    )
+                )
+            } else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive
+                    )
+                )
+            }
+        }
+    }
+}
